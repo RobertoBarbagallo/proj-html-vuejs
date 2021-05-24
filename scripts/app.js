423dfe68d94151bc14ab4f4e6a6c9c924e309605
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-05-24 15:23:19 +0200

    stampa lista navbar

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,37 @@ new Vue({
     el: "#app",
   
     data: {
-    
+      headerList:[
+        {
+          name: "Home",
+          link: "#",
+        },
+        {
+          name: "Pages",
+          link: "#",
+        },
+        {
+          name: "Blog",
+          link: "#",
+        },
+        {
+          name: "Shop",
+          link: "#",
+        },
+        {
+          name: "Shortcodes",
+          link: "#",
+        },
+        {
+          name: "Support",
+          link: "#",
+        },
+        {
+          name: "Contact",
+          link: "#",
+        },
+
+      ],
     },
   
     computed: {
